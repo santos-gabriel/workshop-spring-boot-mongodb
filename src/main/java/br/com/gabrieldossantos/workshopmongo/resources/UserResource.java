4369a0429870796f19033534b1f10a31ffcfe93e
gabriel <gabrielalmeidads@gmail.com>
2021-02-05 22:52:04 -0300

    Adicionando atualização de usuários

--- src/main/java/br/com/gabrieldossantos/workshopmongo/resources/UserResource.java
+++ src/main/java/br/com/gabrieldossantos/workshopmongo/resources/UserResource.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,6 +48,12 @@ public class UserResource {
 	public ResponseEntity<Void> delete(@PathVariable String id){
 		serv.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "{id}")
+	public ResponseEntity<UserDTO> update(@RequestBody UserDTO objDTO, @PathVariable String id){				
+		objDTO.setId(id);
+		return ResponseEntity.ok().body(new UserDTO(serv.update(serv.fromDTO(objDTO))));
 	}
 	
 }
